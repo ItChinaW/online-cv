3c3a7d1e1cb3eef183cc170d3d42f393d0a1893a
wangkai <13245505@qq.com>
2024-05-27 11:27:48 +0800

    init

---
+++ web/pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import {Button} from "antd"
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Button>123</Button>
+        </div>
+    )
+}
